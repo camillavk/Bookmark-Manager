cb59ce8ed70535a34ad353461013e249c4be59bf
Camilla van Klinken <camillavk@gmail.com>
2014-10-07 11:12:24 +0100

    Password confirmation

--- lib/user.rb
+++ lib/user.rb
@@ -12,10 +12,17 @@ class User
 	# and it's not enough for the hash and the salt
 	property :password_digest, Text
 
+	attr_reader :password
+	attr_accessor :password_confirmation
+
+	validates_confirmation_of :password
+
 # when assigned the password, we don't store it directly,
 # insted we generate a password digest and save it in the
 # database. 
 	def password=(password)
+		@password = password
 		self.password_digest = BCrypt::Password.create(password)
 	end
+	
 end
