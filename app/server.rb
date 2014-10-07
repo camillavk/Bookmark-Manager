a8ca70f1dd89beb9b4879101a90a2ad2c1166c49
Camilla van Klinken <camillavk@gmail.com>
2014-10-07 11:43:24 +0100

    Preventing duplicate registrations

--- app/server.rb
+++ app/server.rb
@@ -49,7 +49,7 @@ post '/users' do
 		session[:user_id] = @user.id
 		redirect to('/')
 	else
-		flash[:notice] = "Sorry, your passwords don't match"
+		flash.now[:errors] = @user.errors.full_messages
 		erb :"users/new"
 	end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -5,7 +5,7 @@ class User
 	include DataMapper::Resource
 
 	property :id, Serial
-	property :email, String
+	property :email, String, :unique => true, :message => "This email is already taken"
 	# this will store both the password and the salt
 	# it's text and not string because string holds
 	# 50 characters by default
@@ -15,7 +15,8 @@ class User
 	attr_reader :password
 	attr_accessor :password_confirmation
 
-	validates_confirmation_of :password
+	validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
+	# validates_uniqueness_of :email
 
 # when assigned the password, we don't store it directly,
 # insted we generate a password digest and save it in the
