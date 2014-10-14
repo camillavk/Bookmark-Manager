daf9894a339893e4ff0b6df587073b18d1cc722a
Camilla van Klinken <camillavk@gmail.com>
2014-10-14 17:46:06 +0100

    Data_mapper_setup modified

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,10 +3,19 @@ require './app/models/tag'
 require './app/models/user'
 
 env = ENV["RACK_ENV"] || "development"
-#We're telling datamapper to use a postgres database on localhost. THe name will be "bookmark_manager"
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark_manager_development')
 
-#{"postgres://lpenudetaoyzar:tmGzdzN_KKWem0bRAGl2RZ6IYc@ec2-54-204-43-139.compute-1.amazonaws.com:5432/das4rcsild029d")
+case env
+
+	when env = 'test'
+		path = 'postgres://localhost/bookmark_manager_development'
+	else
+		path = "postgres://lpenudetaoyzar:tmGzdzN_KKWem0bRAGl2RZ6IYc@ec2-54-204-43-139.compute-1.amazonaws.com:5432/das4rcsild029d"
+	end
+
+#We're telling datamapper to use a postgres database on localhost. THe name will be "bookmark_manager"
+DataMapper.setup(:default, path)
+
+#{)
 #{}"postgres://localhost/bookmark_manager_#{env}"}
 
  # this needs to be done after datamapper is initialised
@@ -15,4 +24,4 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark
 DataMapper.finalize
 
 #However, the database tables don't exist yet. Let's tell datamapper to create them
-# DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
